4158e2b5d2d7524f4d8e66b9ee021c1f63e11b25
Alex Tomkins <tomkins@darkzone.net>
2012-10-04 16:57:16 +0100

    Tag to get all categories

--- blanc_basic_events/events/templatetags/events_tags.py
+++ blanc_basic_events/events/templatetags/events_tags.py
@@ -1,7 +1,12 @@
 from django import template
-from blanc_basic_events.events.models import Event
+from blanc_basic_events.events.models import Category, Event
 
 register = template.Library()
+
+
+@register.assignment_tag
+def get_events_categories():
+    return Category.objects.all()
 
 
 @register.assignment_tag

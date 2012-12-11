e5286e023bdac7a8169cf82c002560bec8fcc36b
Alex Tomkins <tomkins@darkzone.net>
2012-12-11 12:23:14 +0000

    Version 0.2.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='blanc-basic-events',
-    version='0.2.3',
+    version='0.2.4',
     description='Blanc Basic Events for Django',
     long_description=open('README.rst').read(),
     url='http://www.blanctools.com/',

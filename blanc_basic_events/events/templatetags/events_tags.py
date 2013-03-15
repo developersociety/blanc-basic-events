4096299061b5f6ea816be9c42791a69352979383
Alex Tomkins <tomkins@darkzone.net>
2013-03-15 14:01:03 +0000

    Add get_upcoming_events template tag

--- blanc_basic_events/events/templatetags/events_tags.py
+++ blanc_basic_events/events/templatetags/events_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blanc_basic_events.events.models import Category, Event
+from blanc_basic_events.events.utils import sorted_event_list
 
 register = template.Library()
 
@@ -12,3 +13,8 @@ def get_events_categories():
 @register.assignment_tag
 def get_events():
     return Event.objects.all().prefetch_related('recurringevent_set', 'recurringeventexclusion_set')
+
+
+@register.assignment_tag
+def get_upcoming_events(limit=None):
+    return sorted_event_list(limit=limit)

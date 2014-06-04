1075f88c1a46c6fbacc74adc6a5b9b26c997be37
Alex Tomkins <tomkins@darkzone.net>
2014-06-04 11:50:11 +0100

    Fix for get_upcoming_events tag using the wrong filter

--- blanc_basic_events/templatetags/events_tags.py
+++ blanc_basic_events/templatetags/events_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 @register.assignment_tag
 def get_upcoming_events(limit=None):
-    event_list = get_special_events_model().objects.filter(final_date__gte=datetime.date.today(),
+    event_list = get_special_events_model().objects.filter(end_date__gte=datetime.date.today(),
                                                            published=True)
 
     if limit is None:
